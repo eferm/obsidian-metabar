defbe186dd0a33c728004f77aa1db34992f8466f
Emanuel Ferm <gemore@gmail.com>
2026-04-07 23:31:05 -0400

    add note-dates-bar plugin

---
+++ README.md
@@ -0,0 +1,9 @@
+# Note Dates Bar
+
+Displays file creation and modification dates in a small bar above the editor.
+
+## Features
+
+- Shows created and modified timestamps at the top of each note in edit mode
+- Uses your system locale for date formatting
+
---
+++ main.js
@@ -0,0 +1,48 @@
+const { Plugin, editorInfoField } = require("obsidian");
+const { showPanel } = require("@codemirror/view");
+
+function formatDate(ms) {
+	const d = new Date(ms);
+	return d.toLocaleString(undefined, {
+		year: "numeric",
+		month: "short",
+		day: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+	});
+}
+
+function updateBar(dom, state) {
+	const info = state.field(editorInfoField, false);
+	const file = info?.file;
+	if (file) {
+		dom.textContent =
+			"Created: " +
+			formatDate(file.stat.ctime) +
+			"  \u00b7  Modified: " +
+			formatDate(file.stat.mtime);
+	} else {
+		dom.textContent = "";
+	}
+}
+
+function createDatePanel(view) {
+	const dom = document.createElement("div");
+	dom.className = "note-dates-bar";
+	updateBar(dom, view.state);
+	return {
+		top: true,
+		dom,
+		update(update) {
+			updateBar(dom, update.state);
+		},
+	};
+}
+
+class NoteDatesBarPlugin extends Plugin {
+	onload() {
+		this.registerEditorExtension(showPanel.of(createDatePanel));
+	}
+}
+
+module.exports = NoteDatesBarPlugin;
